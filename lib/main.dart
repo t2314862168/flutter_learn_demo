a939efaccb1f7a60658d4e5c31cebc0c17311239
t2314862168 <2314862168@qq.com>
2019-08-09 18:21:38 +0800

    新建仓库

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.dark,
+      sized: true,
+      child: MaterialApp(
+        title: 'Login By Pwd',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var topTextStyle = TextStyle(color: Colors.black, fontSize: 18.0);
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+          alignment: Alignment.topRight,
+          child: Text(
+            '验证码登录',
+            style: topTextStyle,
+          ),
+        ),
+      ),
+    );
+  }
+}
